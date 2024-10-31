c930609160ceee56598d91aa1981a2e23dffe848
DarrylWong <darryl@cockroachlabs.com>
2024-10-30 20:08:19 -0400

    add protobufs for controller

---
+++ pkg/ficontroller/main.go
@@ -0,0 +1,33 @@
+// Copyright 2024 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package ficontroller
+
+import (
+	"context"
+
+	_ "github.com/lib/pq" // register postgres driver
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	config := &ControllerConfig{}
+
+	var startCmd = &cobra.Command{
+		Use:   "failure-controller [command] (flags)",
+		Short: "standalone failure-controller tool for conducting failure-injection experiments",
+		Long: `standalone failure-controller tool for conducting failure-injection experiments
+`,
+		Version: "TODO",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			return config.Start(ctx, true)
+		},
+	}
+
+	startCmd.Flags().IntVar(&config.port, "listen-port", 8081, "port to listen on")
+
+}
