ebd9733a32008c339286daff976cecc11a1e6bf4
DarrylWong <darryl@cockroachlabs.com>
2025-09-02 14:52:02 -0400

    add property based test to assert dependency ordering

--- pkg/cmd/roachtest/modular/dag_test.go
+++ pkg/cmd/roachtest/modular/dag_test.go
@@ -3,103 +3,11 @@ package modular
 import (
 	"context"
 	"testing"
-	"time"
+	"path/filepath"
 
-	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/logger"
 	"github.com/cockroachdb/cockroach/pkg/testutils/echotest"
-	"github.com/golang/mock/gomock"
-	"path/filepath"
 )
-
-// TestPlanString tests the String() method of TestPlan.
-func TestPlanner(t *testing.T) {
-	mod := NewTest("basic plan", 12345)
-	ctrl := gomock.NewController(t)
-	mod.WithCreateClusterFn(func() cluster.Cluster {
-		mockCluster := cluster.NewMockCluster(ctrl)
-		mockCluster.EXPECT().Name().Return("my-cluster").AnyTimes()
-		return mockCluster
-	})
-
-	// Add cluster with options
-	_ = mod.AddCluster(
-		MinNodes(3),
-		MaxNodes(5),
-		DisabledDeploymentModes(SeparateProcessDeployment),
-	)
-
-	// Add workload cluster
-	_ = mod.AddWorkloadCluster(WorkloadNodeCount(1))
-
-	baselineStage := mod.NewStage("baseline", DisableFailureInjection())
-	chaosStage := mod.NewStage("chaos")
-
-	// Install prometheus and grafana, then after that is done, start a background loop to scrape metrics.
-	mod.Setup("installing prom" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-		l.Printf("Installing Prometheus and Grafana")
-		return nil
-	})
-	mod.Setup("starting histogram scrape loop" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-		l.Printf("starting histogram scrape loop")
-		time.Sleep(5 * time.Second)
-		l.Printf("measured perf as 400 qps")
-		return nil
-	}, InBackground())
-	mod.Setup("importing tpcc workload" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-		l.Printf("importing TPCC workload")
-		return nil
-	})
-
-	for _, stage := range []*Stage{baselineStage, chaosStage} {
-		// Run TPCC workload
-		mod.InStage(stage, "running TPCC workload for 1 hour" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-			l.Printf("running TPCC workload for 1 hour")
-			return nil
-		}).Then("dropping TPCC tables" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-			l.Printf("dropping TPCC tables")
-			return nil
-		})
-
-		mod.InStage(stage, "increasing replication factor to 5" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-			l.Printf("increasing replication factor to 5")
-			return nil
-		}).Then("waiting for replication" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-			l.Printf("waiting for replication")
-			return nil
-		}).Then("sleeping for 10 minutes" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-			l.Printf("sleeping for 10 minutes")
-			return nil
-		}).Then("decreasing replication factor to 3" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-			l.Printf("decreasing replication factor to 3")
-			return nil
-		})
-
-		mod.InStage(stage, "copy bank table" /* step name */, func(ctx context.Context, l *logger.Logger, h *Helper) error {
-			l.Printf("copy bank table")
-			return nil
-		})
-	}
-
-	// Add after-test step, which is run after prometheus scraping is turned off.
-	mod.AfterTest("TPCC consistency checks", func(ctx context.Context, l *logger.Logger, h *Helper) error {
-		l.Printf("running TPCC consistency checks")
-		return nil
-	})
-
-	// Use the planner to generate the plan with randomized execution strategies
-	planner := NewSimplePlanner(mod, PlannerConfig{
-		IsLocal:           true,
-		ConcurrencyChance: 0.25,
-	})
-
-	plan, err := planner.Plan()
-	if err != nil {
-		t.Fatalf("Failed to generate plan: %v", err)
-	}
-
-	t.Log(plan)
-}
 
 // TestBasicDAG tests the DAG generation using an echo test.
 func TestBasicDAG(t *testing.T) {
